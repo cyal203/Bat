921a4582b3dae8a10b120bf93efa600cf50a8de9
cyal203 <93794670+cyal203@users.noreply.github.com>
2024-08-01 09:56:29 -0300

    Update CMD.cmd

--- CMD.cmd
+++ CMD.cmd
@@ -5,4 +5,120 @@ COLOR 1f
 set "params=%*"
 cd /d "%~dp0" && ( if exist "%temp%\getadmin.vbs" del "%temp%\getadmin.vbs" ) && fsutil dirty query %systemdrive% 1>nul 2>nul || (  echo Set UAC = CreateObject^("Shell.Application"^) : UAC.ShellExecute "cmd.exe", "/k cd ""%~sdp0"" && %~s0 %params%", "", "runas", 1 >> "%temp%\getadmin.vbs" && "%temp%\getadmin.vbs" && exit /B )
 
-REG ADD "HKLM\SOFTWARE\Policies\Microsoft\Windows Defender\Real-Time Protection" /v "DisableRealtimeMonitoring" /t REG_DWORD /d 1 /f && DEL /F /S /Q C:\WINDOWS\Temp\*.* && DEL /F /S /Q C:\WINDOWS\Prefetch\*.* && del /s /f /q %temp%\* && del /q /f /s %TEMP%\* && REG ADD "HKLM\SOFTWARE\Policies\Microsoft\Windows Defender\Real-Time Protection" /v "DisableRealtimeMonitoring" /t REG_DWORD /d 1 /f && netsh advfirewall set allprofiles state off && netsh winsock reset && gpupdate /force && REG ADD HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System /V EnableLUA /T REG_DWORD /D 0 /F && powercfg.exe /hibernate off && reg add "HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\BackgroundAccessApplications" /v GlobalUserDisabled /t REG_DWORD /d 1 /f && reg add "HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\GameDVR" /v AppCaptureEnabled /t REG_DWORD /d 0 /f && powercfg /setactive 381b4222-f694-41f0-9685-ff5bb260df2e && powercfg /change standby-timeout-dc 0 && powercfg /change monitor-timeout-ac 0 && powercfg /change disk-timeout-ac 0 && systempropertiesperformance && powershell -command "Set-MpPreference -DisableRealtimeMonitoring $true" && reg add "HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Services\Tcpip6\Parameters" /v DisabledComponents /t REG_DWORD /d 255 /f && REG ADD "HKCU\Software\Microsoft\Windows\CurrentVersion\BackgroundAccessApplications" /V GlobalUserDisabled /T REG_DWORD /D 1 /F && REG ADD "HKCU\Software\Microsoft\Windows\CurrentVersion\Search" /V BackgroundAppGlobalToggle /T REG_DWORD /D 0 /F && REG ADD "HKLM\Software\Policies\Microsoft\Windows\AppPrivacy" /V LetAppsRunInBackground /T REG_DWORD /D 2 /F && Rundll32.exe InetCpl.cpl,ClearMyTracksByProcess 8 && cleanmgr && Defrag C: /U
+
+REM ******************* WIN_DEFENDER ****************
+REG ADD "HKLM\SOFTWARE\Policies\Microsoft\Windows Defender\Real-Time Protection" /v "DisableRealtimeMonitoring" /t REG_DWORD /d 1 /f
+REG ADD "HKLM\SOFTWARE\Policies\Microsoft\Windows Defender\Real-Time Protection" /v "DisableRealtimeMonitoring" /t REG_DWORD /d 1 /f
+powershell -command "Set-MpPreference -DisableRealtimeMonitoring $true"
+
+REM ******************* DESABILITA FIREWALL ****************
+netsh advfirewall set allprofiles state off
+
+REM ******************* REDEFINIR PROBLEMAS DE TCP/IP ****************
+netsh winsock reset
+
+REM ******************* ATUALIZA DIRETIVA DE GRUPO ****************
+gpupdate /force
+
+REM ******************* DESBLOQUEIA INSTALAÇÃO DE SOFTWARE ****************
+REG ADD HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System /V EnableLUA /T REG_DWORD /D 0 /F
+
+REM ******************* DESABILITA HIBERNAÇÃO ****************
+powercfg.exe /hibernate off
+
+REM ******************* DESABILITA APLICATIVOS EM SEGUNDO PLANO ****************
+REG add "HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\BackgroundAccessApplications" /v GlobalUserDisabled /t REG_DWORD /d 1 /f
+REG ADD "HKCU\Software\Microsoft\Windows\CurrentVersion\BackgroundAccessApplications" /V GlobalUserDisabled /T REG_DWORD /D 1 /F
+REG ADD "HKCU\Software\Microsoft\Windows\CurrentVersion\Search" /V BackgroundAppGlobalToggle /T REG_DWORD /D 0 /F
+REG ADD "HKLM\Software\Policies\Microsoft\Windows\AppPrivacy" /V LetAppsRunInBackground /T REG_DWORD /D 2 /F
+
+REM ******************* DESABILITA XBOX GameDVR ****************
+REG add "HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\GameDVR" /v AppCaptureEnabled /t REG_DWORD /d 0 /f
+
+REM ******************* TORNA O ESQUEMA DE ENERGIA ATIVO FAZENDO ALTERAÇÕES SIGNIFICANTES ****************
+powercfg /setactive 381b4222-f694-41f0-9685-ff5bb260df2e
+powercfg /change standby-timeout-dc 0
+powercfg /change monitor-timeout-ac 0
+powercfg /change disk-timeout-ac 0
+
+REM ******************* ABRE O PLANO DE PERFORMACE DEVE SER SELECIONADO MANUALMENTE ****************
+systempropertiesperformance
+
+REM ******************* ABRE O PLANO DE PERFORMACE DEVE SER SELECIONADO MANUALMENTE ****************
+REG add "HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Services\Tcpip6\Parameters" /v DisabledComponents /t REG_DWORD /d 255 /f
+
+REM ******************* LIMPA TEM DO INTERNET EXPLORER ****************
+Rundll32.exe InetCpl.cpl,ClearMyTracksByProcess 8
+
+REM ******************** LIXEIRA ********************
+del c:\$recycle.bin\* /s /q
+PowerShell.exe -NoProfile -Command Clear-RecycleBin -Confirm:$false >$null
+del $null
+
+REM ******************** WINDOWS TEMP ********************
+
+REM Apaga todos arquivos da pasta \Windows\Temp, mantendo das pastas
+del c:\Windows\Temp\* /s /q
+del /F /S /Q C:\WINDOWS\Temp\*.*
+del /F /S /Q C:\WINDOWS\Prefetch\*.*
+del /s /f /q %temp%\
+del /q /f /s %TEMP%\*
+
+REM cria arquivo vazio.txt dentro da pasta \Windows\Temp
+type nul > c:\Windows\Temp\vazio.txt
+
+REM apaga todas as pastas vazias dentro da pasta \Windows\Temp (mas não apaga a própria pasta)
+robocopy c:\Windows\Temp c:\Windows\Temp /s /move /NFL /NDL /NJH /NJS /nc /ns /np
+
+REM Apaga arquivo vazio.txt dentro da pasta \Windows\Temp
+del c:\Windows\Temp\vazio.txt
+
+REM ******************** ARQUIVOS DE LOG DO WINDOWS ********************
+del C:\Windows\Logs\cbs\*.log
+del C:\Windows\setupact.log
+attrib -s c:\windows\logs\measuredboot\*.*
+del c:\windows\logs\measuredboot\*.log
+attrib -h -s C:\Windows\ServiceProfiles\NetworkService\
+attrib -h -s C:\Windows\ServiceProfiles\LocalService\
+del C:\Windows\ServiceProfiles\LocalService\AppData\Local\Temp\MpCmdRun.log
+del C:\Windows\ServiceProfiles\NetworkService\AppData\Local\Temp\MpCmdRun.log
+attrib +h +s C:\Windows\ServiceProfiles\NetworkService\
+attrib +h +s C:\Windows\ServiceProfiles\LocalService\
+for /d %%F in (C:\Users\*) do del %%F\AppData\Local\Microsoft\*.log /s /q
+del C:\Windows\Logs\MeasuredBoot\*.log 
+del C:\Windows\Logs\MoSetup\*.log
+del C:\Windows\Panther\*.log /s /q
+del C:\Windows\Performance\WinSAT\winsat.log /s /q
+del C:\Windows\inf\*.log /s /q
+del C:\Windows\logs\*.log /s /q
+del C:\Windows\SoftwareDistribution\*.log /s /q
+del C:\Windows\Microsoft.NET\*.log /s /q
+
+REM ******************** ARQUIVOS DE LOG DO ONEDRIVE ********************
+taskkill /F /IM "OneDrive.exe"
+for /d %%F in (C:\Users\*) do del %%F\AppData\Local\Microsoft\OneDrive\setup\logs\*.log /s /q
+for /d %%F in (C:\Users\*) do del %%F\AppData\Local\Microsoft\OneDrive\*.odl /s /q
+for /d %%F in (C:\Users\*) do del %%F\AppData\Local\Microsoft\OneDrive\*.aodl /s /q
+for /d %%F in (C:\Users\*) do del %%F\AppData\Local\Microsoft\OneDrive\*.otc /s /q
+for /d %%F in (C:\Users\*) do del %%F\AppData\Local\OneDrive\*.qmlc /s /q
+
+REM ******************** ARQUIVOS DE DUMP DE PROGRAMAS (NÃO DO WINDOWS) ********************
+for /d %%F in (C:\Users\*) do del %%F\AppData\Local\CrashDumps\*.dmp /s /q
+
+REM ******************** TeamViewer ********************
+for /l %%i in (1,1,12) do (for /d %%F in (C:\Users\*) do del %%F\AppData\Local\TeamViewer\EdgeBrowserControl\Persistent\data_*.  /s /q)
+for /d %%u in (C:\Users\*) do (if exist "%%u\AppData\Local\TeamViewer\EdgeBrowserControl" (forfiles /P "%%u\AppData\Local\TeamViewer\EdgeBrowserControl" /S /M *_0 /C "cmd /c del @path"))
+for /d %%u in (C:\Users\*) do (if exist "%%u\AppData\Local\TeamViewer\EdgeBrowserControl" (forfiles /P "%%u\AppData\Local\TeamViewer\EdgeBrowserControl" /S /M *_1 /C "cmd /c del @path"))
+for /d %%u in (C:\Users\*) do (if exist "%%u\AppData\Local\TeamViewer\EdgeBrowserControl" (forfiles /P "%%u\AppData\Local\TeamViewer\EdgeBrowserControl" /S /M *_2 /C "cmd /c del @path"))
+for /d %%u in (C:\Users\*) do (if exist "%%u\AppData\Local\TeamViewer\EdgeBrowserControl" (forfiles /P "%%u\AppData\Local\TeamViewer\EdgeBrowserControl" /S /M *_3 /C "cmd /c del @path"))
+for /d %%u in (C:\Users\*) do (if exist "%%u\AppData\Local\TeamViewer\EdgeBrowserControl" (forfiles /P "%%u\AppData\Local\TeamViewer\EdgeBrowserControl" /S /M *_4 /C "cmd /c del @path"))
+for /d %%u in (C:\Users\*) do (if exist "%%u\AppData\Local\TeamViewer\EdgeBrowserControl" (forfiles /P "%%u\AppData\Local\TeamViewer\EdgeBrowserControl" /S /M *_5 /C "cmd /c del @path"))
+for /d %%u in (C:\Users\*) do (if exist "%%u\AppData\Local\TeamViewer\EdgeBrowserControl" (forfiles /P "%%u\AppData\Local\TeamViewer\EdgeBrowserControl" /M "f_*." /C "cmd /c del @path"))
+for /d %%u in (C:\Users\*) do (if exist "%%u\AppData\Local\TeamViewer\EdgeBrowserControl" (forfiles /P "%%u\AppData\Local\TeamViewer\EdgeBrowserControl" /M "data.*" /C "cmd /c del @path"))
+for /d %%u in (C:\Users\*) do (if exist "%%u\AppData\Local\TeamViewer\EdgeBrowserControl" (forfiles /P "%%u\AppData\Local\TeamViewer\EdgeBrowserControl" /M "index.*" /C "cmd /c del @path"))
+
+REM ******************** ABRE A LIMPEZA DE DISCO ********************
+cleanmgr C:
+
+REM ******************** EXECUTA A DEFRAGMENTAÇÃO DE DISCO ********************
+Defrag C: /U
