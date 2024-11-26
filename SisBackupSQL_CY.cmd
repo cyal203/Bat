51d950d9863993b6e84e2903acd53a0b7959142a
cyal203 <93794670+cyal203@users.noreply.github.com>
2024-11-26 13:34:19 -0300

    Update SisBackupSQL_CY.cmd

--- SisBackupSQL_CY.cmd
+++ SisBackupSQL_CY.cmd
@@ -3,7 +3,7 @@ chcp 65001 >nul
 title INST BACKUP SQL
 call :VerPrevAdmin
 if "%Admin%"=="ops" goto :eof
-mode con: cols=50 lines=9
+mode con: cols=50 lines=8
 setlocal
 set "params=%*"
 cd /d "%~dp0" && ( if exist "%temp%\getadmin.vbs" del "%temp%\getadmin.vbs" ) && fsutil dirty query %systemdrive% 1>nul 2>nul || (  echo Set UAC = CreateObject^("Shell.Application"^) : UAC.ShellExecute "cmd.exe", "/k cd ""%~sdp0"" && %~s0 %params%", "", "runas", 1 >> "%temp%\getadmin.vbs" && "%temp%\getadmin.vbs" && exit /B )
