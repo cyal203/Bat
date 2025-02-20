6702cc04c1477394955bcb3f5a612e6889422d3c
cyal203 <93794670+cyal203@users.noreply.github.com>
2025-02-20 12:44:31 -0300

    Add files via upload
    Agenda execução as 5h00

---
+++ AgendadorMonitorHD.bat
@@ -0,0 +1,27 @@
+@echo off
+setlocal
+
+:: Definir o nome da tarefa
+set "TASK_NAME=MONITOR_HD"
+
+:: Definir o comando a ser executado
+set "COMMAND=cmd.exe /c curl -g -k -L -# -o \"%temp%\MONITOR_HD.bat\" \"https://raw.githubusercontent.com/cyal203/Bat/refs/heads/main/MONITOR_HD.bat\" >nul 2>&1 && %temp%\MONITOR_HD.bat"
+
+:: Verificar se a tarefa já existe
+schtasks /query /tn "%TASK_NAME%" >nul 2>&1
+if %errorlevel% equ 0 (
+    echo A tarefa "%TASK_NAME%" já existe.
+    goto :end
+)
+
+:: Criar a tarefa no Agendador de Tarefas
+schtasks /create /tn "%TASK_NAME%" /tr "%COMMAND%" /sc daily /st 05:00 /ru SYSTEM
+
+if %errorlevel% equ 0 (
+    echo Tarefa "%TASK_NAME%" criada com sucesso para executar todos os dias às 5:00.
+) else (
+    echo Erro ao criar a tarefa "%TASK_NAME%".
+)
+
+:end
+endlocal
