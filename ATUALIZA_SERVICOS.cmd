2088ddb00fa7b451bfc735205a23a4476d14a651
cyal203 <93794670+cyal203@users.noreply.github.com>
2025-03-05 13:10:09 -0300

    Update ATUALIZA_SERVICOS.cmd

--- ATUALIZA_SERVICOS.cmd
+++ ATUALIZA_SERVICOS.cmd
@@ -1,5 +1,5 @@
 @echo off
-REM -----05/03/2025
+::----05.03.2025
 set "params=%*"
 cd /d "%~dp0" && ( if exist "%temp%\getadmin.vbs" del "%temp%\getadmin.vbs" ) && fsutil dirty query %systemdrive% 1>nul 2>nul || (  echo Set UAC = CreateObject^("Shell.Application"^) : UAC.ShellExecute "cmd.exe", "/k cd ""%~sdp0"" && %~s0 %params%", "", "runas", 1 >> "%temp%\getadmin.vbs" && "%temp%\getadmin.vbs" && exit /B )
 cls
