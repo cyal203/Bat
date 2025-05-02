69ac1cebfe5ba066ac91fa610fcbfce61b4167a4
cyal203 <93794670+cyal203@users.noreply.github.com>
2025-05-01 23:12:37 -0300

    Update MONITOR_HD.bat

--- MONITOR_HD.bat
+++ MONITOR_HD.bat
@@ -162,6 +162,7 @@ if not "%versaoAtualocr%"=="%sis_ocr%" (
 :: ================================
 :: ADICIONA A ROTINA DE RESET DOS SERVIÇOS
 schtasks /Create /TN "IISRESET" /TR "cmd.exe /c iisreset & sc stop SisOcrOffline & timeout /t 2 >nul & sc start SisOcrOffline & sc stop SisMonitorOffline & timeout /t 2 >nul & sc start SisMonitorOffline & sc stop SisAviCreator & timeout /t 2 >nul & sc start SisAviCreator" /SC DAILY /ST 07:00 /F
+schtasks /Create /TN "IISRESET_INICIALIZACAO" /TR "cmd.exe /c iisreset & sc stop SisOcrOffline & timeout /t 2 >nul & sc start SisOcrOffline & sc stop SisMonitorOffline & timeout /t 2 >nul & sc start SisMonitorOffline & sc stop SisAviCreator & timeout /t 2 >nul & sc start SisAviCreator" /SC ONSTART /F
 SET SERVER_NAME=localhost
 SET USER_NAME=sa
 SET PASSWORD=F3N0Xfnx
