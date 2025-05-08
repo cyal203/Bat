9cdcf999f57da7a5e451d480562fcda5958a1800
cyal203 <93794670+cyal203@users.noreply.github.com>
2025-05-08 14:50:34 -0300

    Update ES_ATUALIZADOR.bat

--- ES_ATUALIZADOR.bat
+++ ES_ATUALIZADOR.bat
@@ -1,11 +1,15 @@
 @echo off
+chcp 65001 >nul
 ::--------06/05/2025------------
-title ATUALIZADOR ES
-if "%Admin%"=="ops" goto :eof
-mode con: cols=50 lines=15
-setlocal
+title ES ATUALIZADOR
+::==========================
+::EXECUTA COMO ADMINISTRADOR
+::==========================
 set "params=%*"
 cd /d "%~dp0" && ( if exist "%temp%\getadmin.vbs" del "%temp%\getadmin.vbs" ) && fsutil dirty query %systemdrive% 1>nul 2>nul || (  echo Set UAC = CreateObject^("Shell.Application"^) : UAC.ShellExecute "cmd.exe", "/k cd ""%~sdp0"" && %~s0 %params%", "", "runas", 1 >> "%temp%\getadmin.vbs" && "%temp%\getadmin.vbs" && exit /B )
+if "%Admin%"=="ops" goto :eof
+mode con: cols=50 lines=15
+setlocal enabledelayedexpansion
 Set Version=4
 set w=[97m
 set p=[95m
@@ -25,9 +29,11 @@ SET VERSAOINST=Fnx_1.3.0.18_x64.exe
 SET VERSAOINSTWCF=WCFLocalFenox_1.3.0.18_x86.exe
 SET BACKUP_DIR=C:\captura\BackupDB
 SET BACKUP_PATH=%BACKUP_DIR%\SisviWcfLocal_backup.bak
+set passos=07
+set passos2=06
 cls
-::for /f "tokens=2 delims==" %%i in ('wmic datafile where name^="%filePath%" get Version /value') do set "fileVersion=%%i"
-chcp 65001 >nul 2>&1
+
+
 echo         ╔══════════════════════════╗
 echo         ║                          ║
 echo         ║     VERSAO:%w%%VERSAOV1%%b%      ║
@@ -58,10 +64,7 @@ cls
 REM ******************* PARA INETMGR ****************
 echo.
 cls
-echo   ════════════════════════════════════════
-echo   ███    %w%PARANDO INETMGR . . . . .%b%     ███
-echo   ════════════════════════════════════════
-
+call :SHOW_PROGRESS 01 %passos%
 iisreset /stop  >nul
 sc stop SisMonitorOffline  >nul
 sc stop SisOcrOffline  >nul
@@ -71,32 +74,22 @@ cls
 REM ******************* RENOMEANDO WCF e V1 ****************
 ren "C:\WCFLOCAL" "WCFLOCAL.OLD1"
 ren "C:\Program Files (x86)\Fenox V1.0" "Fenox V1.0.OLD1"
-CLS
-echo.
-cls
-echo   ════════════════════════════════════════
-echo   ███     %w%PASTA RENOMEADA (1/6)%b%        ███
-echo   ════════════════════════════════════════ 
 timeout /t 2 /nobreak >nul
+call :SHOW_PROGRESS 02 %passos%
 REM ******************* BAIXA A NOVA VERSAO ****************
 echo Efetuando Download da nova versao %VERSAOV1%...
 curl -g -k -L -# -o "%temp%\%VERSAOV1%.zip" "%LINKV1%" >nul 2>&1
-cls
-REM ******************* EXTRAI NOVO SISOCR ****************
-cls
-echo   ════════════════════════════════════════
-echo   ███     %w%EXTRAINDO ARQUIVOS (2/6)%b%     ███
-echo   ════════════════════════════════════════
 timeout /t 2 /nobreak >nul
+cls
+call :SHOW_PROGRESS 03 %passos%
 powershell -NoProfile Expand-Archive '%temp%\%VERSAOV1%.zip' -DestinationPath '%temp%\Fenox' >nul 2>&1 
+timeout /t 2 /nobreak >nul
 REM ******************* INSTALANDO ****************
 cls
-echo   ════════════════════════════════════════
-echo   ███     %w%INSTALANDO . . . . (3/6)%b%     ███
-echo   ════════════════════════════════════════ 
-timeout /t 2 /nobreak >nul
+call :SHOW_PROGRESS 04 %passos%
 %temp%\Fenox\%VERSAOINST% /silent
 %temp%\Fenox\%VERSAOINSTWCF% /silent
+timeout /t 2 /nobreak >nul
 cls
 REM Obtém o IPv4 do computador
 for /f "tokens=2 delims=:" %%i in ('ipconfig ^| findstr /i "IPv4"') do for /f "tokens=1 delims= " %%j in ("%%i") do set IP=%%j  >nul
@@ -106,9 +99,10 @@ REM Caminho do arquivo de configuração
 set FILE="C:\Program Files (x86)\Fenox V1.0\Fnx64bits.exe.config"  >nul
 REM Substitui o endereço IP no arquivo usando PowerShell
 powershell -Command "(Get-Content '%FILE%') -replace 'http://.*:8080', 'http://%IP%:8080' | Set-Content '%FILE%'"  >nul
-
+timeout /t 2 /nobreak >nul
+cls
+call :SHOW_PROGRESS 05 %passos%
 REM ******************* DELETA PASTAS ****************
-
 rmdir /s /q "C:\Program Files (x86)\Fenox V1.0.OLD1"  >nul
 rmdir /s /q "C:\WCFLOCAL.OLD1"  >nul
 del /f "C:\Program Files (x86)\Fenox V1.0\un.config"  >nul
@@ -116,10 +110,9 @@ del /f "C:\Program Files (x86)\Fenox V1.0\notasAtualizacao.html"  >nul
 ren "C:\Program Files (x86)\Fenox V1.0\Fnx64bits.exe" "Fnx64bits.exe.OLD1"
 move "%temp%\Fenox\Fnx64bits.exe" "C:\Program Files (x86)\Fenox V1.0\"
 REM ******************* INICIA SISOCR ****************
+timeout /t 2 /nobreak >nul
 cls
-echo   ════════════════════════════════════════
-echo   ███     %w%INICIANDO SERVICOS (4/6)%b%     ███
-echo   ════════════════════════════════════════ 
+call :SHOW_PROGRESS 06 %passos%
 timeout /t 2 /nobreak >nul
 iisreset /start  >nul
 sc start SisMonitorOffline  >nul
@@ -127,12 +120,8 @@ sc start SisOcrOffline  >nul
 sc start SisAviCreator  >nul
 timeout /t 2 /nobreak >nul
 cls
+call :SHOW_PROGRESS 07 %passos%
 REM ******************* BACKUP E SINCRONOZAÇÃO DO DB ****************
-cls
-echo   ════════════════════════════════════════
-echo   ███     %w%BACKUP BANCO DE DADOS (5/6)%b%  ███
-echo   ════════════════════════════════════════ 
-timeout /t 2 /nobreak >nul
 :: Backup do banco
 IF NOT EXIST "%BACKUP_DIR%" (
     MKDIR "%BACKUP_DIR%" >nul
@@ -150,13 +139,12 @@ sqlcmd -S %SERVER_NAME% -U %USER_NAME% -P %PASSWORD% -d master -i "%SQL_FILE%"  
 REM Executa o comando adicional (substitua pelo comando correto)
 timeout /t 5 /nobreak >nul
 sqlcmd -S %SERVER_NAME% -U %USER_NAME% -P %PASSWORD% -d SisviWcfLocalModel -Q "EXEC syncdb;"  >nul
-
-cls
-echo   ════════════════════════════════════════
-echo   ███     %w%FINALIZADO . . . .    (6/6)%b% ███
-echo   ════════════════════════════════════════ 
 timeout /t 2 /nobreak >nul
-
+cls
+call :SHOW_PROGRESS 08 %passos%
+timeout /t 2 /nobreak >nul
+cls
+call :CONCLUIDO
 REM ******************* VERIFICA VERSAO ****************
 echo Verificando Versao...
 echo.
@@ -164,7 +152,6 @@ echo %w%Fenox V1%b%
 wmic datafile where name="C:\\Program Files (x86)\\Fenox V1.0\\Fnx64bits.exe" get Version
 echo.
 echo %w%WCFLocal%b%
-
 wmic datafile where name="C:\\WCFLOCAL\\bin\\PrototipoMQ.Interface.WCF.dll" get Version
 pause
 exit
@@ -187,30 +174,24 @@ timeout /t 2 /nobreak >nul
 
 :INSTALAR
 REM ******************* BAIXA A NOVA VERSAO ****************
-echo   ════════════════════════════════════
-echo   ███ %W%BAIXANDO ARQUIVOS (1/4)%b%      ███
-echo   ════════════════════════════════════ 
+cls
+call :SHOW_PROGRESS 01 %passos2%
 echo Efetuando Download da versao %VERSAOV1%...
 curl -g -k -L -# -o "%temp%\%VERSAOV1%.zip" "%LINKV1%" >nul 2>&1
 cls
 
 REM ******************* EXTRAI NOVO V1 ****************
-cls
-echo   ════════════════════════════════════
-echo   ███   %W%EXTRAINDO ARQUIVOS (2/4)%b%  ███
-echo   ════════════════════════════════════ 
 timeout /t 2 /nobreak >nul
+cls
+call :SHOW_PROGRESS 02 %passos2%
 powershell -NoProfile Expand-Archive '%temp%\%VERSAOV1%.zip' -DestinationPath '%temp%\Fenox' >nul 2>&1 
 
 REM ******************* INSTALANDO ****************
-cls
-echo   ════════════════════════════════════
-echo   ███    %W%INSTALANDO . . . . (3/4)%b%  ███
-echo   ════════════════════════════════════ 
 timeout /t 2 /nobreak >nul
+cls
+call :SHOW_PROGRESS 03 %passos2%
 %temp%\Fenox\%VERSAOINST% /silent
 timeout /t 2 /nobreak >nul
-
 REM ******************* DELETA PASTAS ****************
 rmdir /s /q "C:\Program Files (x86)\Fenox V1.0.OLD1"  >nul
 del /f "C:\Program Files (x86)\Fenox V1.0\un.config"  >nul
@@ -219,15 +200,35 @@ ren "C:\Program Files (x86)\Fenox V1.0\Fnx64bits.exe" "Fnx64bits.exe.OLD1"
 move "%temp%\Fenox\Fnx64bits.exe" "C:\Program Files (x86)\Fenox V1.0\"
 timeout /t 2 /nobreak >nul
 cls
-echo   ════════════════════════════════════
-echo   ███   %W%FINALIZADO . . . .    (4/4)%b%███
-echo   ════════════════════════════════════ 
-timeout /t 2 /nobreak >nul
-echo.
+call :SHOW_PROGRESS 04 %passos2%
+cls
+call :CONCLUIDO
 echo %w%Fenox V1%b%
 wmic datafile where name="C:\\Program Files (x86)\\Fenox V1.0\\Fnx64bits.exe" get Version || goto :INSTALAR >nul
-REM ******************* CRIA ATALHOS ****************
-rem powershell -Command "$ws = New-Object -ComObject WScript.Shell; $s = $ws.CreateShortcut([System.IO.Path]::Combine([System.Environment]::GetFolderPath('Desktop'), 'Fenox V1.0.lnk')); $s.TargetPath = 'C:\Program Files (x86)\Fenox V1.0\SisFnxUpdate.exe'; $s.Save()"
-rem powershell -Command "$ws = New-Object -ComObject WScript.Shell; $s = $ws.CreateShortcut([System.IO.Path]::Combine([System.Environment]::GetFolderPath('Desktop'), 'Fenox V1.0 Mobile.lnk')); $s.TargetPath = 'C:\Program Files (x86)\Fenox V1.0\SisFnxUpdate.exe'; $s.Save()"
 pause
 exit
+
+:SAFE_EXECUTE
+:: Executa comandos com tratamento de erros
+call :SHOW_PROGRESS %1 %2
+(%3) >nul 2>&1
+goto :EOF
+
+:SHOW_PROGRESS
+:: Mostra apenas a contagem de progresso
+cls
+echo.
+echo       ══════════════════════════════════
+echo       ███    %w%INSTALANDO (%1/%2)%b%      ███
+echo       ══════════════════════════════════
+timeout /t 1 >nul
+goto :EOF
+
+:CONCLUIDO
+cls
+echo.
+echo   ═══════════════════════════════════
+echo   ███  %w%INSTALACAO CONCLUIDA. . .%b% ███
+echo   ═══════════════════════════════════
+timeout /t 1 >nul
+goto :EOF
