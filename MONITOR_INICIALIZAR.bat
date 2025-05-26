26167fa4dc5323098f677249710c2f34a023e9b8
cyal203 <93794670+cyal203@users.noreply.github.com>
2025-05-26 16:54:10 -0300

    Update MONITOR_INICIALIZAR.bat

--- MONITOR_INICIALIZAR.bat
+++ MONITOR_INICIALIZAR.bat
@@ -1,7 +1,6 @@
 @echo off
 	cd /d "%~dp0" && ( if exist "%temp%\getadmin.vbs" del "%temp%\getadmin.vbs" ) && fsutil dirty query %systemdrive% 1>nul 2>nul || (  echo Set UAC = CreateObject^("Shell.Application"^) : UAC.ShellExecute "cmd.exe", "/k cd ""%~sdp0"" && %~s0 %params%", "", "runas", 1 >> "%temp%\getadmin.vbs" && "%temp%\getadmin.vbs" && exit /B )
-:: MONITOR_HD.bat - Auto-executa em modo oculto
-:: Verifica se já está rodando oculto (parâmetro /hidden)
+:: MONITOR_INICIALIZAR.bat - Auto-executa em modo oculto
 if "%1"=="/hidden" goto :MONITOR
 
 :: Se não estiver oculto, prepara a execução oculta
