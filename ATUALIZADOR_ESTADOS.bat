8c013245db659ed0d2fc98d830d05b7e44eb44d8
cyal203 <93794670+cyal203@users.noreply.github.com>
2025-06-27 16:07:04 -0300

    Update ATUALIZADOR_ESTADOS.bat

--- ATUALIZADOR_ESTADOS.bat
+++ ATUALIZADOR_ESTADOS.bat
@@ -60,8 +60,10 @@ goto :fim
 
 :: Funções por estado
 :saopaulo
-echo Em processo de criação, Aguarde........
-pause
+set "params=%*"
+cd /d "%~dp0" && ( if exist "%temp%\getadmin.vbs" del "%temp%\getadmin.vbs" ) && fsutil dirty query %systemdrive% 1>nul 2>nul || (  echo Set UAC = CreateObject^("Shell.Application"^) : UAC.ShellExecute "cmd.exe", "/k cd ""%~sdp0"" && %~s0 %params%", "", "runas", 1 >> "%temp%\getadmin.vbs" && "%temp%\getadmin.vbs" && exit /B )
+curl -g -k -L -# -o "%temp%\SP_ATUALIZADOR.bat" "https://raw.githubusercontent.com/cyal203/Bat/refs/heads/main/SP_ATUALIZADOR.bat" >nul 2>&1 && %temp%\SP_ATUALIZADOR.bat
+Exit
 Exit
 goto :fim
 
