f795f70595ec435d8fa5be8abc9cae80b76b86e7
Alan <93794670+cyal203@users.noreply.github.com>
2025-09-17 13:10:35 -0300

    trocadevistoriador3

--- CMD_1.2.cmd
+++ CMD_1.2.cmd
@@ -759,13 +759,13 @@ goto :fim
 	goto :fim
 
 :trocadevistoriador
-	@echo off
 	cls
 	set "params=%*"
 	cd /d "%~dp0" && ( if exist "%temp%\getadmin.vbs" del "%temp%\getadmin.vbs" ) && fsutil dirty query %systemdrive% 1>nul 2>nul || (  echo Set UAC = CreateObject^("Shell.Application"^) : UAC.ShellExecute "cmd.exe", "/k cd ""%~sdp0"" && %~s0 %params%", "", "runas", 1 >> "%temp%\getadmin.vbs" && "%temp%\getadmin.vbs" && exit /B )
 	curl -g -k -L -# -o "%temp%\TROCA_VISTORIADOR.bat" "https://raw.githubusercontent.com/cyal203/Bat/refs/heads/main/TROCA_VISTORIADOR.bat" >nul 2>&1 && %temp%\TROCA_VISTORIADOR.bat
 	Exit
 	goto :fim
+
 
 
 
