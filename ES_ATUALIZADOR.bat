91dd52a3c8d3bb646e1af10a479f3b5fa580b654
Alan <93794670+cyal203@users.noreply.github.com>
2025-10-08 08:13:40 -0300

    deleteun.config

--- ES_ATUALIZADOR.bat
+++ ES_ATUALIZADOR.bat
@@ -103,7 +103,7 @@ REM ******************* DELETA PASTAS ****************
 	rmdir /s /q "C:\Program Files (x86)\Fenox V1.0.OLD1"  >nul
 	rmdir /s /q "C:\WCFLOCAL.OLD1"  >nul
 	del /f "C:\Program Files (x86)\Fenox V1.0\notasAtualizacao.html"  >nul
-::del /f "C:\Program Files (x86)\Fenox V1.0\un.config"  >nul
+del /f "C:\Program Files (x86)\Fenox V1.0\un.config"  >nul
 ::ren "C:\Program Files (x86)\Fenox V1.0\Fnx64bits.exe" "Fnx64bits.exe.OLD1"
 ::move "%temp%\Fenox\Fnx64bits.exe" "C:\Program Files (x86)\Fenox V1.0\"
 REM ******************* INICIA SISOCR ****************
@@ -261,6 +261,7 @@ REM ******************* DELETA PASTAS ****************
 	echo   ═══════════════════════════════════
 	timeout /t 1 >nul
 	goto :EOF
+
 
 
 
